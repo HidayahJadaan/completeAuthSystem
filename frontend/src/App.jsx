647ddf68caf701835a04c709c7f40ed823f21e85
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2026-02-28 14:15:18 +0300

    Finishing first page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// function App() {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+//       <h1 className="text-red-500 text-3xl font-bold">
+//         Hello World
+//       </h1>
+//     </div>
+//   )
+
+import { Route, Routes } from "react-router-dom";
+import FloatingShape from "./components/FloatingShape"
+import SignUpPAge from "./components/pages/SignUpPAge"
+import LoginPage from "./components/pages/LoginPage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    //  <div className="min-h-screen bg-gradient-to-br 
+    //  from-grey-900 via-green-900 to-emerald-900 flex 
+    //  items-center justify-center relative overflow-hidden">
+    <div
+      className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex 
+    items-center justify-center relative overflow-hidden'
+    >
+      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+      <FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
+      <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+
+      <Routes>
+        {/* <Route path="/" element={"Home"} /> */}
+        <Route
+          path='/signup'
+          element={
+
+            <SignUpPAge />
+
+          }
+        />
+        <Route
+          path='/login'
+          element={
+
+            <LoginPage />
+
+          }
+        /></Routes>
+
+
+
+
+
+
+
+
+
+    </div>
+
   )
 }
 
